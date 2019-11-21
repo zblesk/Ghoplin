217c11f09d05d298a4d830acd32d68831a518f42
zblesk <b.ladislav@gmail.com>
2019-11-21 21:59:00 +0100

    Adding the solution

---
+++ Ghoplin/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ghoplin
+{
+    public class Config
+    {
+        public Dictionary<string, BlogConfig> Blogs { get; set; }
+    }
+
+    public class BlogConfig
+    {
+        public string ApiKey { get; set; }
+        public string BlogUrl { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
